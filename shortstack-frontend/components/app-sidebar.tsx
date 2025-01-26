080be269fbc17b85f123cd5d49e7f9ff04b462ea
jessicasewe <jessicasewe89@gmail.com>
2025-01-26 19:24:58 +0000

    feat: dashboard sidear done

--- shortstack-frontend/components/app-sidebar.tsx
+++ shortstack-frontend/components/app-sidebar.tsx
@@ -1,9 +1,10 @@
+"use client";
+
 import {
   ChevronUp,
   Home,
-  Settings,
-  User2,
   Link,
+  User2,
   QrCode,
   PaperclipIcon,
 } from "lucide-react";
@@ -26,32 +27,35 @@ import {
 } from "@radix-ui/react-dropdown-menu";
 import Image from "next/image";
 import shortstack from "../public/shortstack-logo.png";
+import { usePathname } from "next/navigation";
 
 // Menu items.
 const items = [
   {
     title: "Home",
-    url: "#",
+    url: "/dashboard",
     icon: Home,
   },
   {
     title: "Link",
-    url: "#",
+    url: "/dashboard/link",
     icon: Link,
   },
   {
     title: "QR Code",
-    url: "#",
+    url: "/dashboard/qrcode",
     icon: QrCode,
   },
   {
     title: "Page",
-    url: "#",
+    url: "/dashboard/page",
     icon: PaperclipIcon,
   },
 ];
 
 export function AppSidebar() {
+  const pathname = usePathname();
+
   return (
     <Sidebar>
       <SidebarContent>
@@ -61,16 +65,32 @@ export function AppSidebar() {
           </SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
+              {items.map((item) => {
+                // Check if the current item's URL matches the current path
+                const isActive = pathname === item.url;
+
+                return (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <a
+                        href={item.url}
+                        className={`flex items-center space-x-2 px-4 py-2 rounded-md ${
+                          isActive
+                            ? "bg-[#ebd4cd] text-[#a24b33]"
+                            : "text-black hover:bg-[#efeceb]"
+                        }`}
+                      >
+                        <item.icon
+                          className={`w-5 h-5 ${
+                            isActive ? "text-white" : "text-gray-500"
+                          }`}
+                        />
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
