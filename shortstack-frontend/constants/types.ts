31ab66393ec870bf48df62bce41a8af123f93877
jessicasewe <jessicasewe89@gmail.com>
2025-04-02 10:13:19 +0000

    feat:updated templates

--- shortstack-frontend/constants/types.ts
+++ shortstack-frontend/constants/types.ts
@@ -2,3 +2,16 @@ export interface SocialIcon {
   type: "Instagram" | "Twitter" | "Youtube" | "Github" | "Mail";
   username: string;
 }
+
+// Define and export the Template type
+export interface Template {
+  id: string;
+  name: string;
+  image: string;
+  category: string;
+  style: {
+    background: string;
+    buttonStyle: string;
+    textColor: string;
+  };
+}
